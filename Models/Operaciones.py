4a266d5189231a2abf916618d5fc903e1b1fd26b
Tkex555 <bahamutthero@gmail.com>
2024-12-10 10:17:19 -0500

    Update Operaciones.py

--- Models/Operaciones.py
+++ Models/Operaciones.py
@@ -1,16 +1,11 @@
-import mysql.connector
+from Models.conexion import ConexionDB
 
 class OperacionBase:
     def __init__(self):
-        self.db = mysql.connector.connect(
-            host="localhost",     
-            user="root",    
-            password="",  
-            database="calculadora"
-        )
+        conexion_db = ConexionDB()
+        self.db = conexion_db.obtener_conexion()
         self.cursor = self.db.cursor()
 
-    # Guardar en la tabla historial
     def guardar_operacion(self, num1, num2, operador, resultado):
         operacion = f"{num1} {operador} {num2}"
         query = "INSERT INTO historial (operacion, resultado) VALUES (%s, %s)"
@@ -19,7 +14,6 @@ class OperacionBase:
         self.db.commit()
         print("Operación guardada en el historial.")
 
-    # Mostrar las operaciones guardadas en historial
     def mostrar_operaciones(self):
         query = "SELECT * FROM historial"
         self.cursor.execute(query)
@@ -28,6 +22,6 @@ class OperacionBase:
         if resultados:
             print("Historial de operaciones:")
             for operacion in resultados:
-                print(f"{operacion[0]} | {operacion[1]} = {operacion[2]}")
+                print(f"{operacion[1]} = {operacion[2]}")
         else:
             print("No hay operaciones registradas en el historial.")
